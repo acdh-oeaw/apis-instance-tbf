65d706d9e075784e9f291fc567912b78592bf8e3
KK <dev-kk@oeaw.ac.at>
2024-10-22 14:10:58 +0200

    feat(models): add TitlesMixin
    Add abstract class TitlesMixin for entities which need
    title and subtitle fields.

--- apis_ontology/models.py
+++ apis_ontology/models.py
@@ -1,11 +1,36 @@
 from apis_core.apis_entities.models import AbstractEntity
 from apis_core.history.models import VersionMixin
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class BaseEntity(VersionMixin, AbstractEntity):
     """
     Base class for all entities.
     """
+
+    class Meta:
+        abstract = True
+
+
+class TitlesMixin(models.Model):
+    """
+    Mixin for fields shared between work-like entities.
+    """
+
+    title = models.CharField(
+        max_length=255,
+        blank=True,
+        default="",
+        verbose_name=_("Titel"),
+    )
+
+    subtitle = models.CharField(
+        max_length=255,
+        blank=True,
+        default="",
+        verbose_name=_("Untertitel"),
+    )
 
     class Meta:
         abstract = True
