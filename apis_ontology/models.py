53ebf538bd0913781b61a8c9240f04acddff688b
KK <dev-kk@oeaw.ac.at>
2024-10-29 10:00:28 +0100

    feat(models): add Work, Expression classes
    Add a class each for Work and Expression entity.

--- apis_ontology/models.py
+++ apis_ontology/models.py
@@ -34,3 +34,45 @@ class TitlesMixin(models.Model):
 
     class Meta:
         abstract = True
+
+
+class Work(TitlesMixin, BaseEntity):
+    """
+    Distinct intellectual ideas conveyed in artistic and intellectual
+    creations.
+
+    A Work is the outcome of an intellectual process of one or more persons.
+    It exists in a recognisable realisations the form of one or more
+    Expressions (whether finished or partial).
+    Examples: Agatha Christie’s "Murder on the Orient Express" [novel],
+    Alfred Hitchcock's "Psycho" [movie], John Lennon and Paul McCartney's
+    "I want to hold your hand" [song].
+
+    Based on LRMoo class F1 Work:
+    https://www.cidoc-crm.org/extensions/lrmoo/html/LRMoo_v1.0.html#F1
+    """
+
+    class Meta:
+        verbose_name = _("Werk")
+        verbose_name_plural = _("Werke")
+
+
+class Expression(TitlesMixin, BaseEntity):
+    """
+    Intellectual or artistic realisations of Works in the form of identifiable
+    immaterial objects.
+
+    Expressions can be texts, poems, jokes, musical notations, images,
+    multimedia objects etc. as well as combinations of such objects.
+    Examples: the original English text by Agatha Christie for her novel
+    "Murder on the Orient Express"; the German text of "Murder on the Orient
+    Express" as translated by Elisabeth van Bebber and published with the
+    title "Mord im Orientexpress".
+
+    Based on LRMoo class F2 Expression:
+    https://www.cidoc-crm.org/extensions/lrmoo/html/LRMoo_v1.0.html#F2
+    """
+
+    class Meta:
+        verbose_name = _("Expression")
+        verbose_name_plural = _("Expressionen")
