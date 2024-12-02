07c0deb62b85da31f5ec31945389d5705658c81c
KK <dev-kk@oeaw.ac.at>
2024-12-02 10:54:37 +0100

    feat(models): add new Poster fields
    Add various new fields to `Poster` entity class
    needed for import of poster data being prepared
    in OpenRefine.
    
    Closes: #46

--- apis_ontology/models.py
+++ apis_ontology/models.py
@@ -221,6 +221,51 @@ class Poster(BaseEntity):
         verbose_name=_("Titel"),
     )
 
+    storage_location = models.CharField(
+        blank=True,
+        default="",
+        max_length=1024,
+        verbose_name=_("Archivierung"),
+    )
+
+    status = models.CharField(
+        blank=True,
+        default="",
+        max_length=1024,
+        verbose_name=_("Status"),
+    )
+
+    year = models.DateField(
+        blank=True,
+        null=True,
+        verbose_name=_("Jahr"),
+    )
+
+    notes = models.CharField(
+        blank=True,
+        default="",
+        max_length=1024,
+        verbose_name=_("Anmerkungen"),
+    )
+
+    height = models.PositiveSmallIntegerField(
+        blank=True,
+        null=True,
+        verbose_name=_("Länge"),
+    )
+
+    width = models.PositiveSmallIntegerField(
+        blank=True,
+        null=True,
+        verbose_name=_("Breite"),
+    )
+
+    quantity = models.PositiveSmallIntegerField(
+        blank=True,
+        null=True,
+        verbose_name=_("Anzahl"),
+    )
+
     class Meta:
         verbose_name = _("Plakat")
         verbose_name_plural = _("Plakate")
