974b16dc3cb1c76725baab8f0df39397988da951
KK <dev-kk@oeaw.ac.at>
2024-12-02 10:42:22 +0100

    docs(models): update Poster class docstring

--- apis_ontology/models.py
+++ apis_ontology/models.py
@@ -231,7 +231,7 @@ class Poster(BaseEntity):
 
     Typically used for a print product affixed to a vertical surface
     which advertises an upcoming event which may be of interest to
-    viewers/readers/the general public.
+    viewers/readers/the public.
     """
 
     label = models.CharField(
