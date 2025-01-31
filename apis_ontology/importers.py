782a1f32c6b71382dea80ca84cb1fca0ef3582e5
KK <dev-kk@oeaw.ac.at>
2025-01-31 09:11:06 +0100

    feat(importers): lookup Uri, save new Uri
    Check for `Uri` and return `content_object` if it
    already exists. If it doesn't, create new `Uri`
    for newly created entity object.

--- apis_ontology/importers.py
+++ apis_ontology/importers.py
@@ -1,6 +1,8 @@
+from apis_core.apis_metainfo.models import Uri
 from apis_core.generic.importers import GenericModelImporter
 from apis_core.utils.helpers import create_object_from_uri
 from django.apps import apps
+from django.contrib.contenttypes.models import ContentType
 
 
 class BaseEntityImporter(GenericModelImporter):
@@ -14,19 +16,38 @@ class BaseEntityImporter(GenericModelImporter):
 
     def create_instance(self):
         data = self.get_data(drop_unknown_fields=False)
-        model_fields = [field.name for field in self.model._meta.fields]
-        data_for_fields = {key: data[key] for key in data if key in model_fields}
-        subj = self.model.objects.create(**data_for_fields)
-        related_keys = [
-            (x, x.split("__")[1], x.split("__")[2]) for x in data.keys() if "__" in x
-        ]
-        for rk in related_keys:
-            key, obj, rel = rk
-            related_model = apps.get_model("apis_ontology", obj)
-            relation_type = apps.get_model("apis_ontology", rel)
-            if key in data:
-                related_obj = create_object_from_uri(data[key], related_model)
-                relation_type.objects.create(subj=subj, obj=related_obj)
+
+        # lookup existing URI
+        is_uri = Uri.objects.filter(uri=self.import_uri)
+
+        if is_uri:
+            # there should only be one object associated with each URI
+            subj = is_uri.first().content_object
+
+        else:
+            model_fields = [field.name for field in self.model._meta.fields]
+            data_for_fields = {key: data[key] for key in data if key in model_fields}
+
+            subj = self.model.objects.create(**data_for_fields)
+
+            Uri.objects.create(
+                uri=self.import_uri,
+                content_type=ContentType.objects.get_for_model(self.model),
+                object_id=subj.pk,
+            )
+
+            related_keys = [
+                (x, x.split("__")[1], x.split("__")[2])
+                for x in data.keys()
+                if "__" in x
+            ]
+            for rk in related_keys:
+                key, obj, rel = rk
+                related_model = apps.get_model("apis_ontology", obj)
+                relation_type = apps.get_model("apis_ontology", rel)
+                if key in data:
+                    related_obj = create_object_from_uri(data[key], related_model)
+                    relation_type.objects.create(subj=subj, obj=related_obj)
 
         return subj
 
