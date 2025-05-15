2e8f4f7e1ca9b40be520487559443bfc224040dc
KK <dev-kk@oeaw.ac.at>
2025-05-15 12:52:17 +0200

    feat(settings): remove relations from INSTALLED_APPS
    Remove `apis_core.relations` from `INSTALLED_APPS`
    setting in project since it's already being added
    by `apis_acdhch_default_settings`, whose settings
    we are (re)using.

--- apis_ontology/settings.py
+++ apis_ontology/settings.py
@@ -18,9 +18,7 @@ DEBUG = False
 
 # Application definition
 
-APIS_APPS_PREPEND = [
-    "apis_core.relations",
-]
+APIS_APPS_PREPEND = []
 APIS_APPS_APPEND = [
     "apis_core.documentation",
     "django_interval",
