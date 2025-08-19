f603eaf41cc4f038df3eccf6735c838bb87e4575
KK <dev-kk@oeaw.ac.at>
2025-08-19 11:14:32 +0200

    feat(filters): expand parent filterset class for i18n
    Update `BaseEntityFilterSet` class to modify the default
    combined search field included in `AbstractEntityFilterSet`
    so the form field's `label` and `help_text` are also
    translatable/translated (in addition to its `verbose_name`).

--- apis_ontology/filtersets.py
+++ apis_ontology/filtersets.py
@@ -1,6 +1,8 @@
 import logging
 
 from apis_core.apis_entities.filtersets import AbstractEntityFilterSet
+from django.utils.text import format_lazy
+from django.utils.translation import gettext_lazy as _
 
 from apis_ontology.forms import WorkFilterSetForm
 
@@ -14,7 +16,35 @@ class BaseEntityFilterSet(AbstractEntityFilterSet):
     Applies settings to all entity list views (filter sidebars).
     """
 
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if getattr(self.Meta, "model", False):
+            if "search" in self.filters:
+                s_filter = self.filters["search"]
+                s_filter.label = _("Search across text fields")
+
+                if (help_text := s_filter.extra.get("help_text")) and help_text.split(
+                    ":"
+                ):
+                    fields_string = help_text.split(":")[1]
+                    fields_list = fields_string.split(",")
+
+                    # exclude Thomas Bernhard in translation-specific fields
+                    # from combined search field
+                    fields_list_filtered = ", ".join(
+                        [
+                            f
+                            for f in fields_list
+                            if ("in translation" not in f and "TBit" not in f)
+                        ]
+                    )
+
+                    prefix = _("Searches in:")
+
+                    s_filter.extra["help_text"] = format_lazy(
+                        "{prefix} {fields}", prefix=prefix, fields=fields_list_filtered
+                    )
 
 
 class WorkFilterSet(BaseEntityFilterSet):
