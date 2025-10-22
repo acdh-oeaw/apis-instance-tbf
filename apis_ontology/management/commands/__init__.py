13fc8efa7569693c8529b9c88677eb1fa6c47e3b
KK <dev-kk@oeaw.ac.at>
2025-10-22 15:37:21 +0200

    feat(mgmt): add constants to __init__.py
    Add constants for GND and Wikidata URLs to
    `__init__.py` file for management commands.

---
+++ apis_ontology/management/commands/__init__.py
@@ -0,0 +1,5 @@
+GND_URL = "https://d-nb.info/gnd/"
+WIKIDATA_URL = "http://www.wikidata.org/entity/"
+
+TB_GND_ID = "118509861"  # GND ID for Thomas Bernhard
+TB_WD_ID = "Q44336"  # Wikidata ID for Thomas Bernhard
