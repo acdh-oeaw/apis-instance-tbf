14075f7b67e3fa752ec9dd3b8eb8735d8e271dd5
KK <dev-kk@oeaw.ac.at>
2025-11-04 13:40:11 +0100

    feat(api/tbit): add API endpoint for TBit publications
    Add `ManifestationSerializer` and `ManifestationViewSet` classes,
    add route `publications` to `urls.py`.
    In  `ManifestationViewSet`, override `__init__` method to use the
    `name` "Publication" rather than "Manifestation" in the various
    endpoint headers.

--- apis_ontology/urls.py
+++ apis_ontology/urls.py
@@ -3,12 +3,14 @@ from django.urls import include, path
 from rest_framework import routers
 
 from apis_ontology.api.tbit.views import (
+    ManifestationViewSet,
     WorkViewSet,
 )
 
 router = routers.DefaultRouter()
 
 router.register(r"works", WorkViewSet, basename="work")
+router.register(r"publications", ManifestationViewSet, basename="publication")
 
 urlpatterns += [
     path("api/tbit/", include((router.urls, "apis_ontology"))),
