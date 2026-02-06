7a01d0eef953ee8a6fa93bc8c38e791872002a1b
KK <dev-kk@oeaw.ac.at>
2026-02-06 12:55:57 +0100

    feat(urls): add urlpatterns for django-interval

--- apis_ontology/urls.py
+++ apis_ontology/urls.py
@@ -1,5 +1,7 @@
 from apis_acdhch_default_settings.urls import urlpatterns
+from django.urls import include, path
 
 from .api.tbit.urls import urlpatterns as tbit_urls
 
+urlpatterns += [path("", include("django_interval.urls"))]
 urlpatterns += tbit_urls
