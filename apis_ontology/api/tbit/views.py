4a470ba4f4f6f7816ef921c7d0e855271e4f135d
KK <dev-kk@oeaw.ac.at>
2026-02-11 13:21:53 +0100

    feat(api/tbit): add pagination to viewsets
    Create `TbitPagination` class (stub), which inherits from
    `LimitOffsetPagination`, and set `pagination_class` on all
    `ViewSet` classes to it.

--- apis_ontology/api/tbit/views.py
+++ apis_ontology/api/tbit/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.pagination import LimitOffsetPagination
 
 from apis_ontology.models import (
     Manifestation,
@@ -13,17 +14,28 @@ from .serializers import (
 )
 
 
+class TbitPagination(LimitOffsetPagination):
+    """
+    Default pagination class for TBit API endpoints.
+    """
+
+    pass
+
+
 class WorkViewSet(viewsets.ReadOnlyModelViewSet):
+    pagination_class = TbitPagination
     serializer_class = WorkSerializer
     queryset = Work.objects.all().exclude(tbit_category__exact="")
 
 
 class PublicationViewSet(viewsets.ReadOnlyModelViewSet):
+    pagination_class = TbitPagination
     serializer_class = ManifestationSerializer
     queryset = Manifestation.objects.all().exclude(tbit_shelfmark__exact="")
 
 
 class TranslatorViewSet(viewsets.ReadOnlyModelViewSet):
+    pagination_class = TbitPagination
     serializer_class = PersonIsTranslatorSerializer
     queryset = PersonIsTranslatorOfExpression.objects.order_by(
         "subj_object_id"
