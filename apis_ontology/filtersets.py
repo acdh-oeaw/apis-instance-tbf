9fce8551f1fed1cabe9d54c1163f1b32b05c7ac5
KK <dev-kk@oeaw.ac.at>
2026-07-14 15:31:03 +0200

    feat(filtersets): rename, update import for inherited filterset
    `AbstractEntityFilterSet` was renamed to `EntityFilterSet` and
    moved to the new `entities` app in Core `v0.65.0`. This commit
    updates the name and import location of the inherited class.

--- apis_ontology/filtersets.py
+++ apis_ontology/filtersets.py
@@ -1,6 +1,6 @@
 import logging
 
-from apis_core.apis_entities.filtersets import AbstractEntityFilterSet
+from apis_core.entities.filtersets import EntityFilterSet
 from django.utils.text import format_lazy
 from django.utils.translation import gettext_lazy as _
 from django_filters import ChoiceFilter
@@ -10,14 +10,14 @@ from apis_ontology.forms import BaseFilterSetForm, WorkFilterSetForm
 logger = logging.getLogger(__name__)
 
 
-class BaseEntityFilterSet(AbstractEntityFilterSet):
+class BaseEntityFilterSet(EntityFilterSet):
     """
     Parent FilterSet class for all entity classes.
 
     Applies settings to all entity list views (filter sidebars).
     """
 
-    class Meta(AbstractEntityFilterSet.Meta):
+    class Meta(EntityFilterSet.Meta):
         form = BaseFilterSetForm
 
     def __init__(self, *args, **kwargs):
