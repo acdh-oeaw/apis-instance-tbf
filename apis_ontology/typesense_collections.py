12f8d71bfb7dcc5faf9334658c936522b24b3490
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2026-09-02 13:09:12 +0000

    fix(typesense): add sorting fields; change perf serialization
    moves actors and directors to inline objects in performances;
    pulls the publication year from manifestations also in the work
    collection; adds several fields for faceting.
    
    resolves #690

--- apis_ontology/typesense_collections.py
+++ apis_ontology/typesense_collections.py
@@ -6,9 +6,10 @@ from apis_typesense.fields import (
     SameAsField,
     TypesenseField,
 )
+from apis_typesense.models import ModelField, ModelSerializer
 from django.contrib.postgres.expressions import ArraySubquery
 from django.db.models import OuterRef, Subquery, Value
-from django.db.models.functions import Concat
+from django.db.models.functions import Concat, JSONObject
 
 from apis_ontology.models import (
     Expression,
@@ -37,10 +38,9 @@ transl_of = PersonIsTranslatorOfExpression.objects.filter(
     obj_object_id=OuterRef("pk")
 ).values_list("subj_object_id", flat=True)
 
-works = Work.objects.all().annotate(
-    author=ArraySubquery(author_of),
-)
-manifest = Manifestation.objects.filter(id=OuterRef("subj_object_id"))[:1]
+manifest = Manifestation.objects.filter(id=OuterRef("subj_object_id")).order_by(
+    "publication_date"
+)[:1]
 exp_man = ManifestationEmbodiesExpression.objects.filter(
     obj_object_id=OuterRef("id")
 ).annotate(
@@ -57,11 +57,26 @@ expressions = Expression.objects.all().annotate(
 )
 
 persons = Person.objects.all().annotate(label=Concat("forename", Value(" "), "surname"))
+persons_rel = Person.objects.filter(pk=OuterRef("obj_object_id")).annotate(
+    label=Concat("forename", Value(" "), "surname")
+)
 
 perf_work = PerformancePerformedWork.objects.filter(subj_object_id=OuterRef("id"))[:1]
-perf_direct = PerformanceHadDirectorPerson.objects.filter(subj_object_id=OuterRef("id"))
-perf_actor = PerformanceHadParticipantPerson.objects.filter(
-    subj_object_id=OuterRef("id")
+perf_direct = (
+    PerformanceHadDirectorPerson.objects.filter(subj_object_id=OuterRef("id"))
+    .annotate(
+        aut_id=Subquery(persons_rel[:1].values("id")),
+        aut_label=Subquery(persons_rel[:1].values("label")),
+    )
+    .values(json=JSONObject(id="aut_id", label="aut_label"))
+)
+perf_actor = (
+    PerformanceHadParticipantPerson.objects.filter(subj_object_id=OuterRef("id"))
+    .annotate(
+        aut_id=Subquery(persons_rel[:1].values("id")),
+        aut_label=Subquery(persons_rel[:1].values("label")),
+    )
+    .values(json=JSONObject(id="aut_id", label="aut_label"))
 )
 poster_perf = PosterPromotedEvent.objects.filter(obj_object_id=OuterRef("id"))
 perf_group = PerformanceHadParticipantGroup.objects.filter(
@@ -69,18 +84,40 @@ perf_group = PerformanceHadParticipantGroup.objects.filter(
 )
 performance = Performance.objects.all().annotate(
     work_id=Subquery(perf_work.values("obj_object_id")),
-    directors=ArraySubquery(perf_direct.values_list("obj_object_id", flat=True)),
-    actors=ArraySubquery(perf_actor.values_list("obj_object_id", flat=True)),
+    directors=ArraySubquery(perf_direct),
+    actors=ArraySubquery(perf_actor),
     posters=ArraySubquery(poster_perf.values_list("subj_object_id", flat=True)),
     theaters=ArraySubquery(perf_group.values_list("obj_object_id", flat=True)),
 )
+man_work_year = WorkIsRealisedInExpression.objects.filter(
+    subj_object_id=OuterRef("id")
+).annotate(
+    year=Subquery(
+        ManifestationEmbodiesExpression.objects.filter(
+            obj_object_id=OuterRef("obj_object_id")
+        )
+        .annotate(
+            year=Subquery(manifest.values("publication_date")),
+        )
+        .order_by("year")
+        .values("year")[:1]
+    )
+)
+works = Work.objects.all().annotate(
+    author=ArraySubquery(author_of), year=Subquery(man_work_year.values("year")[:1])
+)
+
+
+class PersonModel(ModelSerializer):
+    id: TypesenseField = TypesenseField(type="string", field_name="id")
+    name: TypesenseField = TypesenseField(type="string", field_name="label")
 
 
 class WorkCollection(BaseCollection):
     id: TypesenseField = TypesenseField(type="string", field_name="pk")
     title: TypesenseField = TypesenseField(type="string", sort=True, field_name="title")
     category: TypesenseField = TypesenseField(
-        type="string", field_name="tbit_category", optional=True
+        type="string", field_name="tbit_category", optional=True, facet=True
     )
     author_ids: TypesenseField = TypesenseField(
         type="string[]",
@@ -90,10 +127,14 @@ class WorkCollection(BaseCollection):
         async_reference=True,
         cascade_delete=False,
     )
+    year: TypesenseField = TypesenseField(
+        type="int32", optional=True, field_name="year", sort=True, facet=True
+    )
     sameas: SameAsField = SameAsField(domain="tb-online.acdh-dev.oeaw.ac.at")
 
     default_models = [(works, {"filter": {}, "exclude": {}})]
     collection_name = "work"
+    default_sorting_field = "title"
 
 
 class ExpressionCollection(BaseCollection):
@@ -104,12 +145,11 @@ class ExpressionCollection(BaseCollection):
         reference="tbo_work.id",
         async_reference=True,
         cascade_delete=False,
+        facet=True,
     )
     title: TypesenseField = TypesenseField(type="string", sort=True, field_name="title")
     language: EnumField = EnumField(
-        source="index",
-        type="string",
-        field_name="language_man",
+        source="index", type="string", field_name="language_man", facet=True
     )
     type: FixedStringField = FixedStringField(value="expression", type="string")
     translator_ids: TypesenseField = TypesenseField(
@@ -119,13 +159,15 @@ class ExpressionCollection(BaseCollection):
         reference="tbo_person.id",
         async_reference=True,
         cascade_delete=False,
+        facet=True,
     )
     year: TypesenseField = TypesenseField(
-        type="int32", optional=True, field_name="year"
+        type="int32", optional=True, field_name="year", sort=True, facet=True
     )
     sameas: SameAsField = SameAsField(domain="tb-online.acdh-dev.oeaw.ac.at")
     default_models = [(expressions, {"filter": {}, "exclude": {}})]
     collection_name = "expression"
+    default_sorting_field = "title"
 
 
 class PerformanceCollection(BaseCollection):
@@ -136,24 +178,23 @@ class PerformanceCollection(BaseCollection):
         reference="tbo_work.id",
         async_reference=True,
         cascade_delete=False,
+        facet=True,
     )
     title: TypesenseField = TypesenseField(type="string", sort=True, field_name="label")
     type: FixedStringField = FixedStringField(value="performance", type="string")
-    director_ids: TypesenseField = TypesenseField(
-        type="string[]",
+    directors: ModelField = ModelField(
+        type="object[]",
         optional=True,
-        field_name="directors",
-        reference="tbo_person.id",
-        async_reference=True,
-        cascade_delete=False,
+        model=PersonModel(),
+        accessor="directors",
+        facet=True,
     )
-    actor_ids: TypesenseField = TypesenseField(
-        type="string[]",
+    actors: ModelField = ModelField(
+        type="object[]",
         optional=True,
-        field_name="actors",
-        reference="tbo_person.id",
-        async_reference=True,
-        cascade_delete=False,
+        model=PersonModel(),
+        accessor="actors",
+        facet=True,
     )
     poster_ids: TypesenseField = TypesenseField(
         type="string[]",
@@ -162,6 +203,7 @@ class PerformanceCollection(BaseCollection):
         reference="tbo_poster.id",
         async_reference=True,
         cascade_delete=False,
+        facet=True,
     )
     theater_ids: TypesenseField = TypesenseField(
         type="string[]",
@@ -170,39 +212,49 @@ class PerformanceCollection(BaseCollection):
         reference="tbo_group.id",
         async_reference=True,
         cascade_delete=False,
+        facet=True,
     )
 
     dates: FuzzyDateField = FuzzyDateField(field_name="date_range", optional=True)
     sameas: SameAsField = SameAsField(domain="tb-online.acdh-dev.oeaw.ac.at")
     default_models = [(performance, {"filter": {}, "exclude": {}})]
     collection_name = "performance"
+    default_sorting_field = "title"
 
 
 class PersonCollection(BaseCollection):
     id: TypesenseField = TypesenseField(type="string", field_name="pk")
-    name: TypesenseField = TypesenseField(type="string", field_name="label")
+    name: TypesenseField = TypesenseField(type="string", field_name="label", sort=True)
     sameas: SameAsField = SameAsField(domain="tb-online.acdh-dev.oeaw.ac.at")
     default_models = [(persons, {"filter": {}, "exclude": {}})]
     collection_name = "person"
+    default_sorting_field = "name"
 
 
 class GroupCollection(BaseCollection):
     id: TypesenseField = TypesenseField(type="string", field_name="pk")
-    name: TypesenseField = TypesenseField(type="string", field_name="label")
+    name: TypesenseField = TypesenseField(type="string", field_name="label", sort=True)
     sameas: SameAsField = SameAsField(domain="tb-online.acdh-dev.oeaw.ac.at")
     default_models = [(Group.objects.all(), {"filter": {}, "exclude": {}})]
     collection_name = "group"
+    default_sorting_field = "name"
 
 
 class PosterCollection(BaseCollection):
     id: TypesenseField = TypesenseField(type="string", field_name="pk")
-    name: TypesenseField = TypesenseField(type="string", field_name="label")
+    name: TypesenseField = TypesenseField(type="string", field_name="label", sort=True)
     year: TypesenseField = TypesenseField(
-        type="int32", field_name="year", optional=True
+        type="int32", field_name="year", optional=True, sort=True, facet=True
     )
     country: EnumField = EnumField(
-        type="string", field_name="country", source="index", optional=True
+        type="string",
+        field_name="country",
+        source="index",
+        optional=True,
+        facet=True,
+        sort=True,
     )
     sameas: SameAsField = SameAsField(domain="tb-online.acdh-dev.oeaw.ac.at")
     default_models = [(Poster.objects.all(), {"filter": {}, "exclude": {}})]
     collection_name = "poster"
+    default_sorting_field = "name"
